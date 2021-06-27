abb4e4486548f7ea79d79ec30b715b711820189e
Mujirin <mujirin@mail.ugm.ac.id>
2021-06-27 15:04:37 +0700

    widget lifecycle

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print("Init print");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build print');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a Location"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('The counter is $counter'),
+      ),
+    );
+  }
+}

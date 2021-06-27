44b0f79800ab2341759c879c7fad50f023a7183d
Mujirin <mujirin@mail.ugm.ac.id>
2021-06-28 05:47:19 +0700

    custom classes and more about asynchronus function

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
 
@@ -10,40 +9,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    // Make request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Jakarta'));
-    Map data = jsonDecode(response.body);
-    // print(data);
+  String time = 'loading';
 
-    // Get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    print(offset);
-
-    // Create datetime object
-    DateTime now = DateTime.parse(datetime);
-    print("datetiem befor now $now");
-    now = now.add(Duration(hours: int.parse(offset)));
-    print('after Now $now');
-    print('datetime $datetime');
-    print(int.parse(offset));
-
-
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI
+  String time = ''; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // this is the location url for api endpoint
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async {
+    // Make request
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // Get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    print(offset);
+
+    // Create datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time properti
+    time = now.toString();
+
+  }
+}

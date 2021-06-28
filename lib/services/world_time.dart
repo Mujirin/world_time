ddb064de8881c1e32a844e6bb02ec90b5c9a391a
Mujirin <mujirin@mail.ugm.ac.id>
2021-06-28 08:25:10 +0700

    Try and catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,22 +11,27 @@ class WorldTime {
   WorldTime({ required this.location, required this.flag, required this.url });
 
   Future<void> getTime() async {
-    // Make request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
-    // print(data);
 
-    // Get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    print(offset);
+    try {
+      // Make request
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    // Create datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // Get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      print(offset);
 
-    // set the time properti
-    time = now.toString();
+      // Create datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
+      // set the time properti
+      time = now.toString();
+    } catch (e) {
+      print('The error: $e');
+      time = 'The time cannot be determine';
+    }
   }
 }
